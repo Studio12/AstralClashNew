b72302e210007c1ea0332506e4fac483a51d6295
stoalfaro93 <stoalfaro@knights.ucf.edu>
2015-11-16 00:39:25 -0500

    star testing
    Just changing star script stuff

--- Astral Clash/Assets/Scripts/StoScripts/NewStars.cs
+++ Astral Clash/Assets/Scripts/StoScripts/NewStars.cs
@@ -1,54 +1,54 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class StarPickup : MonoBehaviour {
+public class NewStars : MonoBehaviour {
 
 	public GameObject StarSpawner;
-
+	
 	private Rigidbody2D starry;
-
+	
 	public float movementSpeed;
 	public bool isRight;
-
+	
 	public LayerMask isThereWall;
 	public Transform checkingWall;
 	public bool hitWall;
 	public float wallRadius;
-
+	
 	//Other layermask thing
-	//public LayerMask isRaycaster;
-	//public bool hitOther;
-
-
+	public LayerMask isRaycaster;
+	public bool hitOther;
+	
+	
 	void Start(){
 		starry = GetComponent<Rigidbody2D> ();
-
-		switch (Random.Range (0, 2)) {
 		
+		switch (Random.Range (0, 2)) {
+			
 		case 0:
 			isRight = false;
 			break;
-
+			
 		case 1:
 			isRight = true;
 			break;
-		
+			
 		default:
 			break;
-		
+			
 		}
-
+		
 	}
-
+	
 	void Update(){
-
+		
 		hitWall = Physics2D.OverlapCircle (checkingWall.position, wallRadius, isThereWall);
-		//hitOther = Physics2D.OverlapCircle (checkingWall.position, wallRadius, isRaycaster);
+		hitOther = Physics2D.OverlapCircle (checkingWall.position, wallRadius, isRaycaster);
 
 		if (hitWall) {
 			isRight = !isRight;
 		}
-
+		
 		if (isRight) {
 			transform.localScale = new Vector3 (-3, 3, 1);
 			starry.velocity = new Vector2 (-movementSpeed, starry.velocity.y);
@@ -58,10 +58,10 @@ public class StarPickup : MonoBehaviour {
 			starry.velocity = new Vector2(movementSpeed, starry.velocity.y);
 		}
 	}
-
-
+	
+	
 	void OnTriggerEnter2D(Collider2D coll){
-
+		
 		//Star detects collision with player and does stuff
 		if (coll.tag == "Player") {
 			if(coll.name == "FeetCheck" || coll.name == "FeetReal"){
@@ -70,8 +70,8 @@ public class StarPickup : MonoBehaviour {
 				}
 				StarSpawner.GetComponent<StarSpawn> ().curStars--;
 				Destroy (this.gameObject);
-
-
+				
+				
 			} else{
 				if (coll.GetComponent<Fighter> ().stars < coll.GetComponent<Fighter> ().starMax) {
 					coll.GetComponent<Fighter> ().stars++;
@@ -81,12 +81,12 @@ public class StarPickup : MonoBehaviour {
 			}
 		}
 		if (coll.name == "Killbox") {
-		
+			
 			StarSpawner.GetComponent<StarSpawn> ().curStars--;
 			Destroy (this.gameObject);
 
-
+			
 		}
-	}
 
+	}
 }
