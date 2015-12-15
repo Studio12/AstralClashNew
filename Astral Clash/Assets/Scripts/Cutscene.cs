e807615dfc26a0af9eea8e71d98ab31af926eaf0
DoomTay <bluejedi@cfl.rr.com>
2015-12-14 20:54:35 -0500

    Bumped down credits quality, ending cutscenes now go to menu, menu has credits button

--- Astral Clash/Assets/Scripts/Cutscene.cs
+++ Astral Clash/Assets/Scripts/Cutscene.cs
@@ -6,13 +6,8 @@ public class Cutscene : MonoBehaviour {
 	
 	private MovieTexture movie;
 	public string NextScene;
-<<<<<<< HEAD
-	//	AsyncOperation async;
+	AsyncOperation async;
 	
-=======
-//	AsyncOperation async;
-
->>>>>>> refs/remotes/origin/master
 	// Use this for initialization
 	void Start () {
 		Cursor.visible = false;
@@ -22,98 +17,52 @@ public class Cutscene : MonoBehaviour {
 		movie.Play ();
 		movie.wrapMode = TextureWrapMode.Repeat;
 		Debug.Log("About to start coroutine");
-<<<<<<< HEAD
-		//		if (NextScene != "credits" || Application.loadedLevelName != "credits") {
-		//			StartCoroutine ("load");
-		//		}
-=======
-//		if (NextScene != "credits" || Application.loadedLevelName != "credits") {
-//			StartCoroutine ("load");
-//		}
->>>>>>> refs/remotes/origin/master
+		if (NextScene != "credits" || Application.loadedLevelName != "credits") {
+			StartCoroutine ("load");
+		}
 		StartCoroutine(WaitUntilEnd());
 	}
 	
 	// Update is called once per frame
 	void Update () {
-<<<<<<< HEAD
-		//		if (async != null) {
-		//			if(async.progress == .9f){
-		if (Input.GetButton ("Submit")) {
-			Continue ();
-		}
-		//			}
-		//		} else {
-		//		
-		//			if (Input.GetButton ("Submit")) {
-		//				Continue ();
-		//			}
-		//
-		//		}
-=======
-//		if (async != null) {
-//			if(async.progress == .9f){
+		if (async != null) {
+			if(async.progress == .9f){
+				if (Input.GetButton ("Submit")) {
+					Continue ();
+				}
+			}
+		} else {
 			if (Input.GetButton ("Submit")) {
 				Continue ();
 			}
-//			}
-//		} else {
-//		
-//			if (Input.GetButton ("Submit")) {
-//				Continue ();
-//			}
-//
-//		}
->>>>>>> refs/remotes/origin/master
+
+		}
 	}
 	
 	IEnumerator WaitUntilEnd () {
 		yield return new WaitForSeconds(movie.duration);
 		Continue ();
 	}
-<<<<<<< HEAD
 	
-	//	IEnumerator load(){
-	//
-	//		Debug.Log("Load coroutine started");
-	//		async = Application.LoadLevelAsync (NextScene);
-	//		async.allowSceneActivation = false;
-	//
-	//		yield return async;
-	//
-	//	}
+		IEnumerator load(){
+	
+			Debug.Log("Load coroutine started");
+			async = Application.LoadLevelAsync (NextScene);
+			async.allowSceneActivation = false;
+	
+			yield return async;
+	
+		}
 	
 	void Continue () 
 	{
 		if (NextScene.IndexOf ("SP") == -1) {
-			//			Debug.Log ("Loading next scene");
-			//			if(async != null){
-			//				async.allowSceneActivation = true;
-			//			}else{
-			GameManager.ChooseLevel (NextScene);
-			//			}
-=======
-
-//	IEnumerator load(){
-//
-//		Debug.Log("Load coroutine started");
-//		async = Application.LoadLevelAsync (NextScene);
-//		async.allowSceneActivation = false;
-//
-//		yield return async;
-//
-//	}
-
-	void Continue () 
-	{
-		if (NextScene.IndexOf ("SP") == -1) {
-//			Debug.Log ("Loading next scene");
-//			if(async != null){
-//				async.allowSceneActivation = true;
-//			}else{
-			GameManager.ChooseLevel (NextScene);
-//			}
->>>>>>> refs/remotes/origin/master
+			Debug.Log ("Loading next scene");
+			if(async != null){
+				async.allowSceneActivation = true;
+			}else{
+				GameManager.ChooseLevel (NextScene);
+			}
 		}else {
 			GameManager.curMatch.Level = NextScene;
 			GameObject.Find("GameManager").GetComponent<GameManager>().CreateNewMatch(GameManager.curMatch);
