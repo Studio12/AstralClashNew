a6e89eb01ae5c11c7a66626f2ec83f9e2c652d3e
DoomTay <bluejedi@cfl.rr.com>
2015-12-14 20:04:10 -0500

    Merge preparation

--- Astral Clash/Assets/Scripts/Cutscene.cs
+++ Astral Clash/Assets/Scripts/Cutscene.cs
@@ -6,35 +6,38 @@ public class Cutscene : MonoBehaviour {
 	
 	private MovieTexture movie;
 	public string NextScene;
-	AsyncOperation async;
+	//	AsyncOperation async;
 	
 	// Use this for initialization
 	void Start () {
+		Cursor.visible = false;
 		movie = GetComponent<RawImage> ().texture as MovieTexture;
 		movie.wrapMode = TextureWrapMode.Clamp;
 		movie.loop = false;
 		movie.Play ();
 		movie.wrapMode = TextureWrapMode.Repeat;
 		Debug.Log("About to start coroutine");
-		StartCoroutine ("load");
+		//		if (NextScene != "credits" || Application.loadedLevelName != "credits") {
+		//			StartCoroutine ("load");
+		//		}
 		StartCoroutine(WaitUntilEnd());
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (async != null) {
-			if(async.progress == .9f){
-				if (Input.GetButton ("Submit")) {
-					Continue ();
-				}
-			}
-		} else {
-			
-			if (Input.GetButton ("Submit")) {
-				Continue ();
-			}
-			
+		//		if (async != null) {
+		//			if(async.progress == .9f){
+		if (Input.GetButton ("Submit")) {
+			Continue ();
 		}
+		//			}
+		//		} else {
+		//		
+		//			if (Input.GetButton ("Submit")) {
+		//				Continue ();
+		//			}
+		//
+		//		}
 	}
 	
 	IEnumerator WaitUntilEnd () {
@@ -42,25 +45,25 @@ public class Cutscene : MonoBehaviour {
 		Continue ();
 	}
 	
-	IEnumerator load(){
-		
-		Debug.Log("Load coroutine started");
-		async = Application.LoadLevelAsync (NextScene);
-		async.allowSceneActivation = false;
-		
-		yield return async;
-		
-	}
+	//	IEnumerator load(){
+	//
+	//		Debug.Log("Load coroutine started");
+	//		async = Application.LoadLevelAsync (NextScene);
+	//		async.allowSceneActivation = false;
+	//
+	//		yield return async;
+	//
+	//	}
 	
 	void Continue () 
 	{
 		if (NextScene.IndexOf ("SP") == -1) {
-			Debug.Log ("Loading next scene");
-			if(async != null){
-				async.allowSceneActivation = true;
-			}else{
-				GameManager.ChooseLevel (NextScene);
-			}
+			//			Debug.Log ("Loading next scene");
+			//			if(async != null){
+			//				async.allowSceneActivation = true;
+			//			}else{
+			GameManager.ChooseLevel (NextScene);
+			//			}
 		}else {
 			GameManager.curMatch.Level = NextScene;
 			GameObject.Find("GameManager").GetComponent<GameManager>().CreateNewMatch(GameManager.curMatch);
